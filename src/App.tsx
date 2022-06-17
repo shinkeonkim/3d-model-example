2fc89ec8fea880f59f9c2d9aa1ab0a227d2c28ff
shinkeonkim <singun11@kookmin.ac.kr>
2022-06-17 22:09:02 +0900

    feat: gltf loader를 이용해 3d model 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Canvas, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Sky, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
 
-function App() {
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./cat/scene.gltf");
+
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.6} rotation={[0, -Math.PI/2, 0]} />
+    </>
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Canvas className="canvas">
+        <Suspense fallback={null}>
+          <pointLight color="white" intensity={2.3} position={[10, 20, 10]} />
+          <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
+
+          <Model />
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
